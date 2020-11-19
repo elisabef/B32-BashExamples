a0a124b44fa070c96e6ce08894fdccdfceb77a42
Elisabeth <elisabeth_fortin119@hotmail.com>
2020-11-19 15:36:34 -0500

    .

--- 01-hello_world.sh
+++ 01-hello_world.sh
@@ -1,2 +1,15 @@
 #!/bin/bash
 
+#ceci est un commentaire
+
+echo "Salut Antoine"
+#ou
+echo "salut Antoine"
+echo -n "Vive "
+echo "Linux"
+# va ecrire Vive Linux  sans saut de ligne
+
+echo -e "1\n2\n3\n4" #-e interprète les métacaractères
+
+read -p "My name is? nom
+
